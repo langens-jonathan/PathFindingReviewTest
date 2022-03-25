7e204bbdda5fbe465a209b188e65637baeaed794
Jonathan Langens <jonathan.langens@tenforce.com>
2022-03-25 14:12:27 +0100

    initial commit

---
+++ PathFinding/PathFinding/CalculateAll.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PathFinding
+{
+    public class CalculateAll
+    {
+        public static int[,] Calculate()
+        {
+            const int stopX = 8;
+            const int stopY = 3;
+            var matrix = new int[255, 255];
+            for (int i = 0; i < 255; i++)
+            {
+                for (int j = 0; j < 255; j++)
+                {
+                    matrix[i, j] = (int) Math.Sqrt((i - stopX) * (i - stopX) + (j - stopY) * (j - stopY)); ;
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
